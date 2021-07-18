bb494f07b0c41ec384465c7b5623a6b7b3db1b14
Sjl0107 <52736510+Sjl0107@users.noreply.github.com>
2021-07-18 12:42:18 +0800

    Add files via upload

---
+++ game_sanziqi/game_sanziqi/game.c
@@ -0,0 +1,186 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+
+
+void InitBoard(char board[ROW][COL], int row, int col) {
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row;  i++) {
+		for (j = 0; j < col; j++) {
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col) {
+	int i = 0;
+	for (i = 0; i < row; i++) {//按行打印
+		//打印数据
+		//printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+		int j = 0;
+		for (j = 0; j < col; j++) {
+			printf(" %c ", board[i][j]);
+			if (j < col - 1) {
+				printf("|");
+			}
+			
+		}
+		printf("\n");//打印完数据换行
+		//打印分隔行
+		//if(i<row-1)
+		//	printf("---|---|---\n");
+		if (i < row - 1) {
+			for (j = 0; j < col; j++) {
+				printf("---");
+				if (j < col - 1) {
+					printf("|");
+				}
+				
+			}
+		}
+		printf("\n");//打印完分割线换行
+	}
+}
+
+void PlayerMove(char board[ROW][COL], int row, int col) {
+	int x = 0;
+	int y = 0;
+	printf("玩家走：\n");
+	
+	while (1)
+	{
+		printf("请输入坐标：");
+		scanf("%d%d", &x, &y);
+		if (x >= 1 && x <= row && y >= 1 && y <= col) {
+			if (board[x - 1][y - 1] == ' ') {
+				board[x - 1][y - 1] = '*';//落子
+				break;
+			}
+			else
+			{
+				printf("该坐标以被占用，请重新输入\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入\n");
+		}
+	}
+
+}
+
+void ComputerMove(char board[ROW][COL], int row, int col) {
+	printf("电脑走： \n");
+	while (1)
+	{
+		int x = rand() % row;
+		int y = rand() % col;
+		if (board[x][y] == ' ') {
+			board[x][y] = '#';//电脑落子
+			break;
+		}
+	}
+}
+
+static int IsFull(char board[ROW][COL], int row, int col) {
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row;  i++) {
+		for (j = 0; j < col; j++) {
+			if (board[i][j] == ' ') {
+				return 0;//有空
+			}
+		}
+	}
+	return 1;//已满
+}
+
+char CheckWin(char board[ROW][COL], int row, int col) {
+	int i = 0;
+	int j = 0;
+	int count = 0;
+	//行是否相等
+	//for (i = 0; i < row; i++) {
+	//	if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ') {
+	//		return board[i][0];
+	//	}
+	//}
+	for (i = 0; i < row; i++) {
+		count = 0;
+		for (j = 0; j < col - 1; j++) {
+			if (board[i][j] == board[i][j + 1] && board[i][j] != ' ') {
+				count++;
+			}
+			else
+			{
+				break;
+			}
+			if (count == col - 1) {
+				return board[i][0];
+			}
+		}
+	}
+
+	//列
+	//for (i = 0; i < col; i++) {
+	//	if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ') {
+	//		return board[0][i];
+	//	}
+	//}
+	for (i = 0; i < col; i++) {
+		count = 0;
+		for (j = 0; j < row - 1; j++) {
+			if (board[j][i] == board[j+1][i ] && board[j][i] != ' ') {
+				count++;
+			}
+			else
+			{
+				break;
+			}
+			if (count == row - 1) {
+				return board[0][i];
+			}
+		}
+	}
+	 
+	//对角线
+	//if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
+	//	return board[0][0];
+	//}
+	//if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[1][1] != ' ') {
+	//	return board[1][1];
+	//}
+	for (i = 0; i < row-1; i++) {
+		count = 0;
+		if ((board[i][i] == board[i + 1][i + 1])&&(board[0][0]!=' ')) {
+			count++;
+		}
+		else
+		{
+			break;
+		}
+		if (count == row - 1) {
+			return board[0][0];
+		}
+	}
+	for (i = 0; i < row - 1; i++) {
+		count = 0;
+		if (board[i][row-1-i] == board[i + 1][row-2-i] && board[row-1][0] != ' ') {
+			count++;
+		}
+		else
+		{
+			break;
+		}
+		if (count == row - 1) {
+			return board[row-1][0];
+		}
+	}
+
+	//判断平局 棋盘是否满了 是否有空格
+	if (IsFull(board,ROW,COL)==1) {
+		return 'Q';
+	}
+	//不是平局
+	return 'C';
+}
---
+++ game_sanziqi/game_sanziqi/test.c
@@ -0,0 +1,77 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"game.h"
+
+
+void game() {
+	char ret = 0;
+	//printf("game start");
+	char board[ROW][COL] = { 0 };//数组应该初始化为空格
+	InitBoard(board, ROW, COL);//初始化棋盘
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+	//分析：
+	//1、玩家赢 * 2、电脑赢 # 3、平局 Q 4、继续 C
+	//要判断输赢
+	while (1)
+	{
+		//玩家下棋
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board, ROW, COL);
+		//判断输赢
+		ret = CheckWin(board, ROW, COL);
+		if (ret != 'C') {
+			break;
+		}
+		
+		//电脑下棋
+		ComputerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断输赢
+		ret = CheckWin(board, ROW, COL);
+		if (ret != 'C') {
+			break;
+		}
+		
+	}
+	if (ret == '*') {
+		printf("玩家赢\n");
+	}
+	else if(ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+
+void menu() {
+	printf("##########################\n");
+	printf("######### 1、play ########\n");
+	printf("######### 0、exit ########\n");
+	printf("##########################\n");
+}
+
+int main() {
+	int input = 0;
+	srand((unsigned int)time(NULL));//生成随机数
+	do {
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();//三子棋游戏
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("输入错误，请重新输入\n");
+			break;
+		}
+
+	} while (input);
+}
